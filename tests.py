dea18a7bff2feedb9a6b3f60609d9900ad6e6a76
NichtJens <nichtjens@gmail.com>
2015-09-06 14:30:11 +0200

    added a test-suite skeleton

---
+++ tests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import unittest
+
+from vector import scale, dot, cross, Vector
+
+
+class TestCaseMultiplication(unittest.TestCase):
+
+    def test_scale(self):
+        pass
+
+    def test_dot(self):
+        pass
+
+    def test_cross(self):
+        pass
+
+
+class TestCaseVector(unittest.TestCase):
+
+    def test_scale(self):
+        pass
+
+    def test_dot(self):
+        pass
+
+    def test_cross(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
