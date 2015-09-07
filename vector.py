14ecefae124d8eae354fcac2f7e3a143269d23e8
NichtJens <nichtjens@gmail.com>
2015-09-07 17:43:20 +0200

    added code for the multiplication to the vector class

--- vector.py
+++ vector.py
@@ -19,19 +19,24 @@ def cross(A, B):
 
 # x, y, z, and t should be properties of the class
 # further properties: dimension, length (and alternative names for these)
-# conversion from/to spherical coordinates should be possible
 
 class Vector(list):
 
-    def __init__(self, x, y, z, t):
-        pass
+    def __init__(self, x, y, z=None, t=None):
+        list.__init__(self)
+        self += [c for c in (x, y, z, t) if c is not None]
+        # handle alternatives like Vector(L) with L=(1, 2, ...)
+        # handle 4d vectors, t should be zeroth component
+        # spherical and cylindrical coordinates
+
 
     def __repr__(self):
-        pass
+        return repr(tuple(self))
 
 
     def __eq__(self, other):
-        pass
+        return False not in [s == o for s, o in zip(self, other)]
+
 
     def __add__(self, other):
         pass
@@ -39,8 +44,22 @@ class Vector(list):
     def __sub__(self, other):
         pass
 
+
     def __mul__(self, other):
-        pass
+        if type(other) == type(self):
+            return dot(self, other)
+        else:
+            return Vector(*scale(self, other))
+
+    __rmul__ = __mul__
+
+
+    def __mod__(self, other):
+        if type(other) == type(self):
+            return Vector(*cross(self, other))
+        else:
+            return Vector(*[c % other for c in self])
+
 
     def __div__(self, other):
         pass
